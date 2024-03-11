b4403cbb78f170722ccbad41721832fc5dfd9ebe
RajsGithubs <92363295+RajsGithubs@users.noreply.github.com>
2024-03-11 08:33:26 +0000

    variable file added

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,9 @@
+const accountId = 123456
+let accontEmail = "Account@gamil.com"
+var accountPassword = "987654"
+accountCity ="Gwalior"
+
+// accountId = 2
+
+// console.log(accountId);
+console.table([accountId,accontEmail,accountPassword,accountCity]);
