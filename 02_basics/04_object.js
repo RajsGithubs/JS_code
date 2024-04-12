b2ca2e6e19039ae9135dc77f1f20108e90d5c4f9
RajsGithubs <92363295+RajsGithubs@users.noreply.github.com>
2024-04-12 18:38:13 +0000

    object complete

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -48,9 +48,35 @@ const user = [
 
 user[1].email
 
-console.log(FbUser);
+// console.log(FbUser);
 
-console.log(Object.keys(FbUser));
-console.log(Object.values(FbUser));
-console.log(Object.entries(FbUser));
-console.log(FbUser.hasOwnProperty("isLoggenIn"));
+// console.log(Object.keys(FbUser));
+// console.log(Object.values(FbUser));
+// console.log(Object.entries(FbUser));
+// console.log(FbUser.hasOwnProperty("isLoggenIn"));
+
+const course = {
+    coursename : "JS Course",
+    Courceprice : "999",
+    courseinstructor : "Raj"
+}
+
+// course.courseinstructor
+
+const {courseinstructor:instructor} = course
+
+// console.log(courseinstructor);
+console.log(instructor);
+
+// Example of apis
+// {
+//     "name":"raj",
+//     "Coursename": "js course"
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
