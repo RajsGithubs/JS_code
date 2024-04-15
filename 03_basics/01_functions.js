b95a61c575d402974a9c44c4f2891c8a7e7441a9
RajsGithubs <92363295+RajsGithubs@users.noreply.github.com>
2024-04-15 19:31:00 +0000

    function added

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -48,4 +48,34 @@ function LoginUserMessage(username = "User") {  // Another way to give a default
 }
 
 // console.log(LoginUserMessage("Raj"));
-console.log(LoginUserMessage());
+// console.log(LoginUserMessage());
+
+function CalculateKartPrice(...num1) {      //This is also known as rest operater it depends on the basic of where we use it Rest operater uses to bindup or bundle the values
+    return num1
+}
+
+// console.log(CalculateKartPrice(200, 400, 500));
+
+const user = {
+    username : "Raj",
+    price : 9999
+}
+
+function handleobject(anyobject) {
+    console.log(`user name is ${anyobject.username} and total amount is ${anyobject.price}`);
+}
+
+// handleobject(user)
+
+handleobject({
+    username : "Mannu",
+    price : 299
+})
+
+const myNewArray = [100, 200, 300, 400]
+
+function ReturnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(ReturnSecondValue(myNewArray));
