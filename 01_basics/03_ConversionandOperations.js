4eb8e6aa78f179c277c12ff434414c6ccf9e5eb5
RajsGithubs <92363295+RajsGithubs@users.noreply.github.com>
2024-08-08 08:31:19 +0000

    new changes

--- 01_basics/03_ConversionandOperations.js
+++ 01_basics/03_ConversionandOperations.js
@@ -49,3 +49,16 @@ console.log("1" + 2);
 console.log(1 + "2");
 console.log("1" + 2 + 2);
 console.log(1 + 2 + "2");
+
+console.log (( 3 + 4 ) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
